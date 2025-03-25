e84744128162088a79016c6cf300a0f586195fe7
Allef Gomes <allefgalmeida@gmail.com>
2025-03-25 16:11:37 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+// create a interection from 0 to 10000000 and print it
+    // configure to be i64 to avoid overflow
+    for i in 0..10000000 {
+        println!("{}", i);    
+    }
+}
